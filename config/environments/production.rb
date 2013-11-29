3ae926ea807617c9d3bd862225fd9b9c2e6d38a0
João Vieira <shocker@abandonados.info>
2013-11-29 13:35:50 +0000

    Set up SendGrid to send emails in production in Heroku app.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,18 +78,35 @@ DashboardApi::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default :charset => "utf-8"
+  #config.action_mailer.smtp_settings = {
+	#  address: "smtp.gmail.com",
+	#  port: 587,
+	#  domain: "myapp.herokuapp.com",
+	#  authentication: "plain",
+	#  enable_starttls_auto: true,
+	#  user_name: ENV["GMAIL_USERNAME"],
+	#  password: ENV["GMAIL_PASSWORD"]
+  #}
+  
+  # SendGrid configuration
+  config.action_mailer.default_url_options = { :host => 'dashboard-bb.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-	  address: "smtp.gmail.com",
+	  address: "smtp.sendgrid.net",
 	  port: 587,
-	  domain: "myapp.herokuapp.com",
+	  domain: "dashboard-bb.herokuapp.com",
 	  authentication: "plain",
 	  enable_starttls_auto: true,
-	  user_name: ENV["GMAIL_USERNAME"],
-	  password: ENV["GMAIL_PASSWORD"]
+	  user_name: ENV["SENDGRID_USERNAME"],
+	  password: ENV["SENDGRID_PASSWORD"]
   }
+}
 end
