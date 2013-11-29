1ebb9ac6bb02f70694eabc1eb1c6dbde225ba9c0
João Vieira <shocker@abandonados.info>
2013-11-29 15:54:33 +0000

    Changed email template.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,5 +1,5 @@
 class Emailer < ActionMailer::Base
-  default from: "snapshot@example.com", subject: 'Dashboard snapshot'
+  default from: "snap@dashboard-bb.herokuapp.com", subject: 'Dashboard snapshot'
   
   def send_snapshot(image, name, email)
     attachments[name] = image
